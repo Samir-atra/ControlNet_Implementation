9cf30a4301df8fe036c7060a1e2473447c758853
Samer <samiratra95@gmail.com>
2025-06-02 04:28:18 +0300

    partially impelemnting an initial version of the controlnet

--- src/controlnet.py
+++ src/controlnet.py
@@ -1,1 +1,46 @@
 """the ControlNet model architecture"""
+
+
+import keras
+from keras import layers
+import tensorflow as tf
+import keras_hub
+
+## initial implementatin
+
+def get_model(img_size, num_classes):
+    """The ControlNet model with the first half similar to the 
+    latent stable diffusion model a and the second half is 1x1 
+    convolutional layers"""
+    
+    
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
